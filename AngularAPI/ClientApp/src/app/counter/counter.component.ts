4bc75f60121a99090e269f9101b43c30a6893b73
Bogdan Polak <bogdanpol@gmail.com>
2021-03-21 13:52:50 +0000

    Refactored CounterComponent

--- AngularAPI/ClientApp/src/app/counter/counter.component.ts
+++ AngularAPI/ClientApp/src/app/counter/counter.component.ts
@@ -2,23 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter-component',
-  template: `<h1>Two way counter</h1>
+  template:
+`<h1>Two way counter</h1>
 <p aria-live="polite">Current count: <strong>{{ currentCount }}</strong></p>
 <div class="d-flex flex-wrap">
   <button class="btn btn-primary" (click)="incrementCounter()">Increment</button>
-  <incrementor [increment]="increment" (onChange)="onIncChaged($event)"></incrementor>
+  <incrementor [increment]="increment" (onChange)="onIncrementChanged($event)"></incrementor>
 </div>`
 })
 export class CounterComponent {
   public currentCount = 0;
-  increment = 1;
+  public increment = 1;
 
   public incrementCounter() {
-    const inc = +this.increment;
-    this.currentCount += isNaN(inc) ? 0 : inc;
+    this.currentCount += +this.increment;
   }
 
-  onIncChaged(value: number) {
+  onIncrementChanged(value: number) {
     this.increment = value;
   }
 }
