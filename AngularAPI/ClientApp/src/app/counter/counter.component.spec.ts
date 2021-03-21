5a6c7003bb423f8a7ca8b1eed9fb91295529e8ad
Bogdan Polak <bogdanpol@gmail.com>
2021-03-21 21:40:32 +0100

    Fixed unit tests

--- AngularAPI/ClientApp/src/app/counter/counter.component.spec.ts
+++ AngularAPI/ClientApp/src/app/counter/counter.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
+import { IncrementorComponent } from '../incrementor/incrementor.component';
 import { CounterComponent } from './counter.component';
 
 describe('CounterComponent', () => {
@@ -7,7 +9,8 @@ describe('CounterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CounterComponent ]
+      declarations: [ CounterComponent, IncrementorComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
@@ -19,7 +22,7 @@ describe('CounterComponent', () => {
 
   it('should display a title', async(() => {
     const titleText = fixture.nativeElement.querySelector('h1').textContent;
-    expect(titleText).toEqual('Counter');
+    expect(titleText).toEqual('Two way counter');
   }));
 
   it('should start with count 0, then increments by 1 when clicked', async(() => {
@@ -30,5 +33,31 @@ describe('CounterComponent', () => {
     incrementButton.click();
     fixture.detectChanges();
     expect(countElement.textContent).toEqual('1');
+  }));
+
+  it('should increments by 2 when increment click plus', async(() => {
+    const incrementComponent = fixture.nativeElement.getElementsByClassName('increment')[0];
+    const plusButton = incrementComponent.getElementsByTagName('button')[1];
+    plusButton.click();
+
+    const countElement = fixture.nativeElement.querySelector('strong');
+    const incrementButton = fixture.nativeElement.querySelector('button');
+    incrementButton.click();
+    fixture.detectChanges();
+    expect(countElement.textContent).toEqual('2');
+  }));
+
+  it('should decrements by 3 when increment click 3 times minus', async(() => {
+    const incrementComponent = fixture.nativeElement.getElementsByClassName('increment')[0];
+    const minusButton = incrementComponent.getElementsByTagName('button')[0];
+    minusButton.click();
+    minusButton.click();
+    minusButton.click();
+
+    const countElement = fixture.nativeElement.querySelector('strong');
+    const incrementButton = fixture.nativeElement.querySelector('button');
+    incrementButton.click();
+    fixture.detectChanges();
+    expect(countElement.textContent).toEqual('-2');
   }));
 });
